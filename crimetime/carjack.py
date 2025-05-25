bbd220f6e638e4c8164ac84259f9554056caf00a
Vainne <jrdear@gmail.com>
2025-05-25 12:02:57 -0400

    updated ford and oldesmobile into carjack.py

--- crimetime/carjack.py
+++ crimetime/carjack.py
@@ -34,7 +34,11 @@ chevy5 = { "make" : "Chevrolet", "year" : 1973, "model" : "Nova", "max" : 250000
 dodge1 = { "make" : "Dodge", "year" : 1972, "model" : "Charger", "max" : 250000, "value" : 16100,}
 dodge2 = { "make" : "Dodge", "year" : 1970, "model" : "Dart", "max" : 250000, "value" : 52500,}
 dodge3 = { "make" : "Dodge", "year" : 2003, "model" : "Ram 1500", "max" : 250000, "value" : 9109,}
-
+ford1 = { "make" : "Ford", "year" : 1991, "model" : "Mustang GT 5.0 Hatchback", "max" : 250000, "value" : 19650,}
+ford2 = { "make" : "Ford", "year" : 1976, "model" : "Maverick", "max" : 250000, "value" : 9576,}
+ford3 = { "make" : "Ford", "year" : 2020, "model" : "Expedition", "max" : 250000, "value" : 26846,}
+ford4 = { "make" : "Ford", "year" : 2025, "model" : "Explorer", "max" : 250000, "value" : 39785,}
+oldsmobile1 = { "make" : "Oldsmobile", "year" : 1971, "model" : "Vista Cruiser", "max" : 250000, "value" : 23300,}
 
 # Junk Cars:
 junk1 = { "make" : "Chevrolet", "year" : 1982, "model" : "Cavalier", "max" : 250000, "value" : 1450,}
@@ -44,9 +48,16 @@ junk4 = { "make" : "Chevrolet", "year" : 2016, "model" : "Malibu", "max" : 25000
 junk5 = { "make" : "Dodge", "year" : 1970, "model" : "Challenger", "max" : 250000, "value" : 3173,}
 junk6 = { "make" : "Dodge", "year" : 1993, "model" : "Intrepid", "max" : 250000, "value" : 1204,}
 junk7 = { "make" : "Dodge", "year" : 1999, "model" : "Neon", "max" : 250000, "value" : 1226,}
+junk8 = { "make" : "Ford", "year" : 2004, "model" : "F-150", "max" : 250000, "value" : 4032,}
+junk9 = { "make" : "Ford", "year" : 1998, "model" : "Probe", "max" : 250000, "value" : 2080,}
+junk10 = { "make" : "Ford", "year" : 2014, "model" : "Escape", "max" : 250000, "value" : 7981,}
+junk11 = { "make" : "Ford", "year" : 2010, "model" : "Ranger", "max" : 250000, "value" : 7396,}
+junk12 = { "make" : "Oldsmobile", "year" : 1997, "model" : "Custlass", "max" : 250000, "value" : 1569,}
+junk13 = { "make" : "Oldsmobile", "year" : 1992, "model" : "Toronado", "max" : 250000, "value" : 6719,}
+
 # Groupings
 rarest_cars = [rarest1, rarest2, rarest3, rarest4, rarest5, rarest6, rarest7, rarest8, rarest9, rarest10]
 semi_rare_cars = [semirare1, semirare2, semirare3, semirare4, semirare5, semirare6]
-common_cars = [chevy1, chevy2, chevy3, chevy4, chevy5, dodge1, dodge2, dodge3]
-junk_cars = [junk1, junk2, junk3, junk4, junk5, junk6, junk7]
+common_cars = [chevy1, chevy2, chevy3, chevy4, chevy5, dodge1, dodge2, dodge3, ford1, ford2, ford3, ford4, oldsmobile1]
+junk_cars = [junk1, junk2, junk3, junk4, junk5, junk6, junk7, junk8, junk9, junk10, junk11, junk12, junk13]
 all_cars = [rarest_cars, semi_rare_cars, common_cars, junk_cars]
