fc2fe9e75ea7df1aadd303d071f946b47bd420f8
Vainne <jrdear@gmail.com>
2025-05-25 11:17:35 -0400

    updated an inf error in carjack.py

--- crimetime/carjack.py
+++ crimetime/carjack.py
@@ -26,17 +26,17 @@ semirare5 = { "make" : "Ford", "year" : 1932, "model" : "T-Bucket Roadster", "ma
 semirare6 = { "make" : "Porsche", "year" : 1965, "model" : "911", "max" : 3300, "value" : 205000,}
 
 # Common Cars:
-chevy1 = { "make" : "Chevrolet", "year" : 2004, "model" : "Corvette", "max" : float(inf), "value" : 17650,}
-chevy2 = { "make" : "Chevrolet", "year" : 1965, "model" : "Impala", "max" : float(inf), "value" : 15041,}
-chevy3 = { "make" : "Chevrolet", "year" : 2012, "model" : "Silverado", "max" : float(inf), "value" : 13974,}
-chevy4 = { "make" : "Chevrolet", "year" : 2022, "model" : "Tahoe", "max" : float(inf), "value" : 41989,}
-chevy5 = { "make" : "Chevrolet", "year" : 1973, "model" : "Nova", "max" : float(inf), "value" : 10610,}
+chevy1 = { "make" : "Chevrolet", "year" : 2004, "model" : "Corvette", "max" : 250000, "value" : 17650,}
+chevy2 = { "make" : "Chevrolet", "year" : 1965, "model" : "Impala", "max" : 250000, "value" : 15041,}
+chevy3 = { "make" : "Chevrolet", "year" : 2012, "model" : "Silverado", "max" : 250000, "value" : 13974,}
+chevy4 = { "make" : "Chevrolet", "year" : 2022, "model" : "Tahoe", "max" : 250000, "value" : 41989,}
+chevy5 = { "make" : "Chevrolet", "year" : 1973, "model" : "Nova", "max" : 250000, "value" : 10610,}
 
 # Junk Cars:
-junk1 = { "make" : "Chevrolet", "year" : 1982, "model" : "Cavalier", "max" : float(inf), "value" : 1450,}
-junk2 = { "make" : "Chevrolet", "year" : 2001, "model" : "Camaro", "max" : float(inf), "value" : 2449,}
-junk3 = { "make" : "Chevrolet", "year" : 2001, "model" : "S-10", "max" : float(inf), "value" : 4989,}
-junk4 = { "make" : "Chevrolet", "year" : 2016, "model" : "Malibu", "max" : float(inf), "value" : 9987,}
+junk1 = { "make" : "Chevrolet", "year" : 1982, "model" : "Cavalier", "max" : 250000, "value" : 1450,}
+junk2 = { "make" : "Chevrolet", "year" : 2001, "model" : "Camaro", "max" : 250000, "value" : 2449,}
+junk3 = { "make" : "Chevrolet", "year" : 2001, "model" : "S-10", "max" : 250000, "value" : 4989,}
+junk4 = { "make" : "Chevrolet", "year" : 2016, "model" : "Malibu", "max" : 250000, "value" : 9987,}
 # Groupings
 rarest_cars = [rarest1, rarest2, rarest3, rarest4, rarest5, rarest6, rarest7, rarest8, rarest9, rarest10]
 semi_rare_cars = [semirare1, semirare2, semirare3, semirare4, semirare5, semirare6]
